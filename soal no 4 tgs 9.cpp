5209c0cff26870fcf7d56e0cd5faa7225852d8e3
Mayvan Gandhy <33478920+LahatArmy@users.noreply.github.com>
2017-11-28 12:33:07 +0700

    Update soal no 4 tgs 9.cpp

--- soal no 4 tgs 9.cpp
+++ soal no 4 tgs 9.cpp
@@ -1,15 +1,43 @@
 #include<iostream>
 using namespace std;
 
+//Fungsi untuk membentuk array
+int** getArrayElement() {
+	//Membuat array 2 dimensi
+	int** arr = new int*[5];
+	
+	//Inisialisasi nilai variabel angka
+	int angka = 2;
+	
+	for(int i = 0; i < 5; i++) {
+		arr[i] = new int[5];
+		
+		for(int j = 0; j < 5; j++) {
+			
+			//Mengisi array
+			arr[i][j] = angka + j;
+		}
+		
+		//Setiap selesai melooping kolom, tambah nilai angka
+		angka++;
+	}
+	
+	//Mengembalikan nilai array
+	return arr;
+}
+
 main() {
+	//Mendapatkan array element dari fungsi
+	int** arr = getArrayElement();
+	
 	//Looping pertama untuk menampilkan 5 baris dimulai dari i = 2 sampai i <= 6
-	for(int i = 2; i <= 6; i++) {
+	for(int i = 0; i < 5; i++) {
 		
 		//Looping kedua untuk menampilkan 5 kolom tiap 1 baris
 		for(int j = 0; j < 5; j++) {
 			
 			//Menampilkan nilai
-			cout<<i + j<<" ";
+			cout<<arr[i][j]<<" ";
 		}
 		
 		//Membuat baris baru setelah looping 10 kolom
