51cec1655f449191d82fe58fd9aeb9289a7c8079
Mayvan Gandhy <33478920+LahatArmy@users.noreply.github.com>
2017-11-28 12:04:44 +0700

    Update soal no 2 tgs 9.cpp

--- soal no 2 tgs 9.cpp
+++ soal no 2 tgs 9.cpp
@@ -1,15 +1,36 @@
-#include<iostream>
-#include<iomanip> 
-using namespace std;
+ using namespace std;
 
-main() 
+// Prototype
+int *generateArray(int number);
+// fungsi umum
+int length = 10;
+
+int main()
 {
-	for(int i = 1; i <= 10; i++) {		
-		
-		for(int j = 1; j <= i; j++) {
-	
-			cout<<setw(3)<<i * j<<" ";
-		}
-		cout<<endl;
-	}
+  int **array2d = new int*[length];
+  for (int i = 0; i < length; i++) {
+    array2d[i] = new int[length];
+    // simpan array 1d ke 2d
+    array2d[i] = generateArray(i + 1);
+  }
+
+  // tampilkan array 2d
+  for (int i = 1; i <= length; i++) {
+    for (int j = 0; j < i; j++) {
+      cout << array2d[i - 1][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
+// Fungsi umum array 1d
+int *generateArray(int number)
+{
+  int *numbersList = new int[length];
+
+  for (int i = 1; i <= length; i++) {
+    numbersList[i - 1] = number * i;
+  }
+
+  return numbersList;
 }
