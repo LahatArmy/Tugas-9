63eec14f82b9ce9104d4891d90a344de3475cbef
Mayvan Gandhy <33478920+LahatArmy@users.noreply.github.com>
2017-11-28 12:35:38 +0700

    Update soal no 5 tugas 9.cpp

--- soal no 5 tugas 9.cpp
+++ soal no 5 tugas 9.cpp
@@ -1,8 +1,32 @@
 #include<iostream>
 using namespace std;
 
+//Fungsi untuk membentuk array
+int* getArrayElement() {
+	//Membuat array 1 dimensi
+	int* arr = new int[15];
+	
+	//Inisialiasi nilai angka
+	int angka = 2;
+	
+	for(int i = 0; i < 15; i++) {
+		//Mengisi array
+		arr[i] = angka;
+		
+		//Setiap selesai melooping kolom, tambah nilai angka
+		angka+=2;
+	}
+	
+	//Mengembalikan nilai array
+	return arr;
+}
+
 main() {
-	for(int i = 2; i <= 30; i+=2) {
-		cout<<i<<" ";
+	//Mendapatkan array element dari fungsi
+	int* arr = getArrayElement();
+	
+	//Looping menampilkan isi array
+	for(int i = 0; i < 15; i++) {
+		cout<<arr[i]<<" ";
 	}
 }
